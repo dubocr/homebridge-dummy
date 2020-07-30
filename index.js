819062c2e296a861f785cdbcb8dd997c46500b15
Romain DUBOC <dubocr@gmail.com>
2020-07-30 22:54:15 +0200

    Multiple switches state machine

--- index.js
+++ index.js
@@ -15,49 +15,68 @@ function DummySwitch(log, config) {
   this.name = config.name;
   this.stateful = config.stateful;
   this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this._service = new Service.Switch(this.name);
-  
+  this.time = config.time ? config.time : 1000;	
+  this.states = config.states ? config.states : [config.name];
+  this._services = [];
+
   this.cacheDirectory = HomebridgeAPI.user.persistPath();
   this.storage = require('node-persist');
   this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
-  
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
 
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
+  for(let i=0; i < this.states.length; i++) {
+    const service = new Service.Switch(this.states[i], 'state_'+i);
+    this._services.push(service);
 
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
+    service.getCharacteristic(Characteristic.On)
+      .on('set', this._setOn.bind(this, i));
+
+    if (this.reverse) service.setCharacteristic(Characteristic.On, true);
+
+    if (this.stateful) {
+      var cachedState = this.storage.getItemSync(service.displayName);
+      if((cachedState === undefined) || (cachedState === false)) {
+        service.setCharacteristic(Characteristic.On, false);
+      } else {
+        service.setCharacteristic(Characteristic.On, true);
+      }
+
+      if(this._services.length > 1) {
+        this._services[0].setCharacteristic(Characteristic.On, !this.reverse);
+      }
+    }
   }
 }
 
 DummySwitch.prototype.getServices = function() {
-  return [this._service];
+  return this._services;
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+DummySwitch.prototype._setOn = function(i, on, callback) {
 
   this.log("Setting switch to " + on);
 
   if (on && !this.reverse && !this.stateful) {
     setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
+      this._services[i].setCharacteristic(Characteristic.On, false);
     }.bind(this), this.time);
   } else if (!on && this.reverse && !this.stateful) {
     setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
+      this._services[i].setCharacteristic(Characteristic.On, true);
     }.bind(this), this.time);
   }
   
   if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
+    this.storage.setItemSync(this._services[i].displayName, on);
+    
+    if((on && !this.reverse) || (!on && this.reverse)) {
+      for(let j=0; j < this._services.length; j++) {
+        if(j != i) {
+          this._services[j].getCharacteristic(Characteristic.On).updateValue(!on);
+          this.storage.setItemSync(this._services[j].displayName, !on);
+        }
+      }
+    }
   }
-  
+
   callback();
 }
